d25efeb952e6e1d01e0a55f595f094dcfe309805
Vapasi <vapasi@in-Vapasi.local>
2019-04-26 12:46:31 +0530

    adding sel scripts

--- src/test/java/tests/SimpleLogin.java
+++ src/test/java/tests/SimpleLogin.java
@@ -2,37 +2,45 @@ package tests;
 
 
 
+import javafx.scene.layout.Priority;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
+import static org.testng.Assert.assertTrue;
 
-public class SimpleLoginTest {
+
+public class SimpleLogin extends Basic
+{
 
     ChromeDriver driver;
 
 
 
     @Test
-    public void testLogin(){
+    public void testLogin() {
 
 
-        String currentUsersWorkingDir = System.getProperty("user.dir");
-        System.out.println("Dir is " + currentUsersWorkingDir);
-        System.setProperty("webdriver.chrome.driver",currentUsersWorkingDir+"/src/test/resources/chromedriver");
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.navigate().to("https://spree-vapasi.herokuapp.com");
-        driver.navigate().to("https://spree-vapasi.herokuapp.com");
+        //driver.navigate().to("https://spree-vapasi.herokuapp.com");
         driver.findElement(By.id("link-to-login")).click();
         driver.findElement(By.id("spree_user_email")).sendKeys("spree@example.com");
         driver.findElement(By.id("spree_user_password")).sendKeys("spree123");
         driver.findElement(By.name("commit")).click();
-        driver.close();
-        driver.quit();
+        //WebElement str= driver.findElement(By.xpath("//*[@id=\"nav-bar\"]/li[2]/a"));
+        //String value= str.getText();
+        assertTrue(driver.findElement(By.linkText("My Account")).isDisplayed());
     }
+
+
+
+
+
 
 
 
